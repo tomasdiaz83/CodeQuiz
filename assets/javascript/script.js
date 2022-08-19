2de3e9a094922c9d197aa8776c6816d7e26597e5
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-08-19 14:09:06 -0500

    HTML draft  skeleton built out, some script pseudo

---
+++ assets/javascript/script.js
@@ -0,0 +1,30 @@
+//click start button listener
+//timer starts 
+//prompts with questions
+//if answers = wrong then subtract timer
+//if answer = correct then continue prompts
+//if all questions completed && timer = 0 then end game
+//localStorage for the scores 
+//input for saving initials
+
+// var for all selectors - 
+
+// var time
+// var win
+// var loss
+// var button
+// etc.
+
+// button start
+
+// function starting timer
+// function winning
+// function loss
+// function initials
+// function showing all the quiz questions
+// function submit the input in the form for initials
+
+// //storing data
+// function storing wins
+// function storing losses
+// function storing intiials
